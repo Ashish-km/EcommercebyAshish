b084083539ac0bdce1a1ab6f52921a19a190d6d4
Ashish Kushwah <ashishkushawh888@gmail.com>
2024-02-02 11:14:19 +0530

    first

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../services/admin.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
 
+  user_dashboard_data: any;
+  total_user: number = 0;
+  admin_user: number = 0;
+  seller_user: number = 0;
+  buyer_user: number = 0;
+        
+  product_dashboard_data: any;
+  total_product: number = 0;
+  publish_product: number = 0;
+  inactive_product: number = 0;
+  draft_product: number = 0;
+  constructor(private router: Router, private adminService: AdminService) { }
+  ngOnInit(): void {
+    this.adminProductDashboard();
+    this.adminUserDashboardData();
+  }
+
+  userDashboard() {
+    this.router.navigateByUrl("/admin/user")
+  }
+
+  productDashboard() {
+    this.router.navigateByUrl("/admin/product")
+  }
+  //in this fuction we willl at service section and call the function and get the userdata form json file 
+
+  adminUserDashboardData() {
+    this.adminService.userDashbordData().subscribe(data => {
+      this.user_dashboard_data = data;
+      console.log(this.user_dashboard_data)
+      for (let user in this.user_dashboard_data) {
+        if (this.user_dashboard_data[user].role == 'admin') {
+          ++this.admin_user;
+        }
+        else if (this.user_dashboard_data[user].role == 'seller') {
+          ++this.seller_user;
+        }
+        else if (this.user_dashboard_data[user].role == 'buyer') {
+          ++this.buyer_user;
+        }
+        ++this.total_user;
+      }
+    }, error => {
+      console.log("my error: ", error);
+    })
+  }
+
+
+  adminProductDashboard() {
+    this.adminService.productDashbordData().subscribe(data => {
+      this.product_dashboard_data = data;
+      console.log(this.product_dashboard_data)
+      for (let status in this.product_dashboard_data) {
+        if (this.product_dashboard_data[status].status == 'publish') {
+          ++this.publish_product;
+        }
+        else if (this.product_dashboard_data[status].status == 'inactive') {
+          ++this.inactive_product;
+        }
+        else if (this.product_dashboard_data[status].status == 'darft') {
+          ++this.draft_product;
+        }
+        ++this.total_product;
+      }
+    }
+      , error => {
+        console.log(' my error', error)
+      })
+  }
 }
--- src/app/admin/admin-login/admin-login.component.ts
+++ src/app/admin/admin-login/admin-login.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginSignupService } from '../../shared/services/login-signup.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './admin-login.component.html',
   styleUrl: './admin-login.component.scss'
 })
-export class AdminLoginComponent {
+export class AdminLoginComponent implements OnInit {
+  signInFormValue: any = {};
+  user_data: any = {};
+  constructor(private router: Router, private loginService: LoginSignupService) {
+  }
+  ngOnInit(): void {
 
+  }
+  onSubmitSignIn() {
+    this.loginService.adminLogin(this.signInFormValue.userEmail, this.signInFormValue.userPassword).subscribe(data => {
+      this.user_data = data;
+      if (this.user_data.legnth = 1) {
+        sessionStorage.setItem('user_Session_id', this.user_data[0].id);
+        sessionStorage.setItem("role", this.user_data[0].role);
+        this.router.navigateByUrl('/admin-dashboard');
+      }
+      else {
+        alert("Invalid Response")
+      }
+      console.log(this.user_data);
+    }, error => {
+      console.log("MY error", error)
+    })
+  }
 }
