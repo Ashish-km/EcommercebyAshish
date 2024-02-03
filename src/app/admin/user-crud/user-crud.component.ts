2598e6a5b35e84fd5fd06d865a83fa9a153f3b3f
Ashish Kushwah <ashishkushawh888@gmail.com>
2024-02-03 23:02:11 +0530

    lp

--- src/app/admin/user-crud/user-crud.component.ts
+++ src/app/admin/user-crud/user-crud.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { User } from '../../core/Model/object-model';
 
 @Component({
   selector: 'app-user-crud',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-crud.component.html',
   styleUrl: './user-crud.component.scss'
 })
 export class UserCrudComponent {
+ 
+   all_user_data:any;
+   single_user_data:any;
+   addEditUserForm!:FormGroup;
+   user_dto!:User;
+  user_reg_data:any;
+  edit_user_id:any;
+  upload_file_name!:string;
+  adddEditUser:boolean=false;
+  
+
+
 
 }
